1e52b1411aa782328eea36011b8d16823e358595
leobalter <leonardo.balter@gmail.com>
2023-03-02 16:49:52 -0800

    import James' example

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,22 +1,47 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 const resolve = Symbol('resolveConnectedCallback');
 const connected = Symbol('connectedCallback');
 const loading = Symbol('loading component');
 
 export default class App extends LightningElement {
-  @track dynamic = { activity: undefined, details: undefined };
-
   [loading] = false;
   [resolve] = false;
   [connected] = new Promise(res => this[resolve] = res);
+
+  componentConstructor;
+  tabs = [
+      {
+          key: 0,
+          name: 'Home',
+          cmp: 'home'
+      },
+      {
+          key: 1,
+          name: 'Activities',
+          cmp: 'activities'
+      },
+      {
+          key: 2,
+          name: 'Details',
+          cmp: 'details'
+      },
+  ];
 
   connectedCallback() {
     this[resolve]();
   }
 
-  async loadComponents() {
+  async loadComponents({ target: { value }}) {
     if (this[loading]) {
+      return;
+    }
+
+    const cmp = this.tabs[value].cmp;
+
+    // Uses cached component if it's already loaded
+    if (typeof cmp !== 'string') {
+      this.componentConstructor = cmp;
       return;
     }
 
@@ -25,16 +50,13 @@ export default class App extends LightningElement {
     await this[connected];
 
     // Uses the default export from the LWC
-    const [ activity, details ] = (await Promise.all([
-      import('x/activity'),
-      import('x/details'),
-    ])).map(ns => ns.default);
-
-    //const [ activity, details ] = loaded.map(ns => ns.default);
+    const { defalt: loaded } = await import(`x/${cmp}`);
 
     // the LWC constructor is used by the lwc:is attribute of <lwc:component />
-    this.dynamic.activity = activity;
-    this.dynamic.details = details;
+    this.componentConstructor = loaded;
+
+    // Store loaded component to avoid new imports
+    his.tabs[value].cmp = loaded;
 
     // reloading is now available;
     this[loading] = false;
